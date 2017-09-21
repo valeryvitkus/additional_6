caafbba1a9d1ba6ec979662395e4f9dc41b0e274
Valery Vitkys <vitkys91@gmail.com>
2017-09-21 21:33:44 +0300

    add factorials function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function zeros(expression) {
-  // your solution
+  
+  function factorial(n) {
+    if (n==0) {
+      return 1;
+    } else {
+      return n * factorial(n-1);
+    }
+  }
+  
+  function doublefactorial(n) {
+    if (n==0) {
+      return 1;
+    } else {
+      return n * factorial(n-2);
+    }
+  }
+  
 }
